6d393a306f232f9af2d0e26d979a9b0b9e697599
CoolStar <1848505943@qq.com>
2023-12-18 23:09:42 +0800

    add basic components

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -2,12 +2,24 @@ import { useCallback } from "react";
 import { FaFeather } from "react-icons/fa";
 import { useRouter } from "next/router";
 
+import useLoginModal from "@/hooks/useLoginModal";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 const SidebarTweetButton = () => {
   const router = useRouter();
+  const loginModal = useLoginModal();
+  const { data: currentUser } = useCurrentUser();
+
+  const onClick = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    router.push('/');
+  }, [loginModal, router, currentUser]);
 
   return (
-    <div>
+    <div onClick={onClick}>
       <div className="
         mt-6
         lg:hidden 
